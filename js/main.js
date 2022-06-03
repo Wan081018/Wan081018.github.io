5467e5f1abd294e115e67d782229e37021dcd150
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-06-03 16:03:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+function display(class_name){
+	class_name.setAttribute("display", "inherit");
+	return;
+}
+
 function search_it(){
 	var url;
+	display(document.getElementsByClassName("search_div")[0]);
 	window.location.href = url;
 	return;
 }
 
-function display_it() {
+function display_it(){
+	display(document.getElementsByClassName("article_list")[0]);
 	return;
 }
