7bc768e3c26833aec100ddaa3471ee29ddcf3f13
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-06-03 09:27:15 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function search_it(){
-	var url = prompt(
-		"Please enter which article you want to go:",
-		"article"
-	);
-	if(url=="" || url==null) return;
+	var url;
 	window.location.href = url;
+	return;
+}
+
+function display_it() {
 	return;
 }
